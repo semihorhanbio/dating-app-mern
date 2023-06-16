4d6a8cf52381fde5d7ee38ab7c57574cb7e5290c
Semih Orhan <sorhan3.14@gmail.com>
2023-06-16 14:37:49 +0300

    create DatingCards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import DatingCards from './components/DatingCards';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <DatingCards/>
     </div>
   );
 }
