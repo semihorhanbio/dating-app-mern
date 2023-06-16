9963b5258162009143ab0a2487ab136779b43e4a
Semih Orhan <sorhan3.14@gmail.com>
2023-06-16 15:17:39 +0300

    create initial route

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+//App Config
+const app = express()
+const port = process.env.PORT || 8001
+
+//Middleware
+
+//DB Config
+
+//API Endpoints
+app.get('/', (req, res) => {
+    res.send("Hello TheWebDev")
+})
+
+//Listener
+app.listen(port, () => console.log(`Listening on localhost: ${port}`))
