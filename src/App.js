4fb8719b632ec0f6222f63a532231afcdeedc5e2
Semih Orhan <sorhan3.14@gmail.com>
2023-06-16 13:56:35 +0300

    created Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Dating App MERN</h1>
+      <Header />
     </div>
   );
 }
