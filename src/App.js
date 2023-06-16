d9dfeb8cc20436b69bc3c24f95838897456d7c1d
Semih Orhan <sorhan3.14@gmail.com>
2023-06-16 14:56:32 +0300

    create swipebuttons component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import DatingCards from './components/DatingCards';
+import SwipeButtons from './components/SwipeButtons';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="app">
       <Header />
       <DatingCards/>
+      <SwipeButtons/>
     </div>
   );
 }
